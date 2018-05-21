0b851e5c6550a23a70d32fed19c1dca8dbaa8546
Dan <theloncar@gmail.com>
2018-05-22 00:14:45 +0200

    components-services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Smokey Grilled Bacon',
+      'Mmmmmm that Bacon Smells Delicious',
+      'https://c1.staticflickr.com/7/6141/5974874958_b95e1e1c65_b.jpg',
+      [
+        new Ingredient('Bacon', 6),
+        new Ingredient('Spicy Sauce', 1),
+        
+      ]),
+    new Recipe(
+      'Big Bacon Sandwich',
+      'Tacky and Tasteful',
+      'https://c1.staticflickr.com/7/6158/6175755733_b2932d7838_b.jpg',
+    [
+      new Ingredient('Bacon', 4),
+      new Ingredient('Chesse', 4),
+      
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
